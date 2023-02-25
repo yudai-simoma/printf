f1474371e014db1f687a89b22b924b0fa7686bfe
yudai-shimoma <yshimoma@student.42tokyo.jp>
2023-02-25 18:47:23 +0900

    雛形作成途中

--- ft_printf.c
+++ ft_printf.c
@@ -6,37 +6,103 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 11:47:10 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/02/25 12:48:58 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/02/25 18:42:47 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-//
-//1, エラーチェック（引数カウント）
-//2, %が来るまで出力
-//3, %の次の文字で処理を分ける
-//
-//
+//文字列をput_strに格納する関数
+void	str_add_str(char *put_str, char *add_str)
+{
+	while (*add_str != '\0')
+	{
+		*put_str = *add_str;
+		put_str++;
+		add_str++;
+	}
+}
+
+//文字をput_strに格納する変数
+char	*char_add_str(char *put_str, char c)
+{
+	*put_str = c;
+	return (put_str);
+}
+
+void	ap_add_str(char *put_str, const char *format, va_list *ap)
+{
+	if (*format == 'c')
+		*put_str = (char)va_arg(*ap, int);
+	else if (*format == 's')
+		str_add_str(put_str, (char *)va_arg(*ap, char *));
+	// else if (*format == 'P')
+
+	// else if (*format == 'd')
+
+	// else if (*format == 'i')
+
+	// else if (*format == 'u')
+
+	// else if (*format == 'x')
+
+	// else if (*format == 'X')
+
+	else if (*format == '%')
+		*put_str = '%';
+	else
+		*put_str = (char)va_arg(*ap, int);
+	// put_str++;
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
+	char	*put_str;
+	char	*head;
+	size_t	return_num;
 	size_t	i;
+	size_t	j;
 
+	put_str = (char *)calloc(((size_t)INT_MAX + 1), sizeof(char));
+	if (put_str == NULL)
+		return (0);
+	head = put_str;
 	va_start(ap, format);
 	i = 0;
+	j = 0;
 	while (format[i] != '\0')
 	{
+		if (format[i] == '%')
+			//csdixXpの対応をする
+			ap_add_str(put_str, &format[++i], &ap);
+		else
+			//文字をput_strに格納する
+			*put_str = format[i];
 		i++;
+		put_str++;
 	}
 	va_end(ap);
-	return (0);
+	return_num = ft_strlen(head);
+	if (write(1, head, return_num) == -1)
+		return (0);
+	// free(put_str);
+	head = NULL;
+	if (return_num > INT_MAX)
+		return (0);
+	else
+		return ((int)return_num);
 }
 
 #include <stdio.h>
 int	main(void)
 {
-	printf("%c\n", 'b');
-	ft_printf("%c", 'a');
+	int a,b;
+
+	a = printf("printf: %c, %s\n", 'b', "12345678");
+	b = ft_printf("ft_printf: %c, %s\n", 'b', "12345678");
+	// a = printf("   printf: %c%c%%%c\n", 'b', '1', 'p');
+	// b = ft_printf("ft_printf: %c%c%%%c\n", 'b', '1', 'p');
+	printf ("\nprintf:%d, ft_printf:%d", a, b);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 12:58:55 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/02/25 12:47:49 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/02/25 13:56:01 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_PRINTF_H
 # include <stdarg.h>
 # include <unistd.h>
+# include <limits.h>
 # include "libft/libft.h"
 
 int	ft_printf(const char *format, ...);
