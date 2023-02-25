be90216a0da6d6f32f2618a69b0291939890017e
yudai-shimoma <yshimoma@student.42tokyo.jp>
2023-02-25 19:57:37 +0900

    構造体の使用

--- ft_printf2.c
+++ ft_printf2.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 11:47:10 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/02/25 19:55:59 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/02/25 19:48:07 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 //文字列をput_strに格納する関数
-void	str_add_str(char **put_str, char *add_str)
+void	str_add_str(t_str *str, char *add_str)
 {
 	while (*add_str != '\0')
 	{
-		**put_str = *add_str;
-		put_str++;
+		*str->put_str = *add_str;
+		str->put_str++;
 		add_str++;
 	}
 }
 
 //文字をput_strに格納する変数
-void	char_add_str(char **put_str, char c)
+void	char_add_str(t_str *str, char c)
 {
-	**put_str = c;
-	put_str++;
+	*str->put_str = c;
+	str->put_str++;
 }
 
-void	ap_add_str(char **put_str, const char *format, va_list *ap)
+void	ap_add_str(t_str *str, const char *format, va_list *ap)
 {
 	if (*format == 'c')
-		char_add_str(put_str, (char)va_arg(*ap, int));
+		char_add_str(str, (char)va_arg(*ap, int));
 	else if (*format == 's')
-		str_add_str(put_str, (char *)va_arg(*ap, char *));
+		str_add_str(str, (char *)va_arg(*ap, char *));
 	// else if (*format == 'P')
 
 	// else if (*format == 'd')
@@ -49,33 +49,33 @@ void	ap_add_str(char **put_str, const char *format, va_list *ap)
 	// else if (*format == 'X')
 
 	else if (*format == '%')
-		char_add_str(put_str, '%');
+		char_add_str(str, '%');
 	else
-		char_add_str(put_str, (char)va_arg(*ap, int));
+		char_add_str(str, (char)va_arg(*ap, int));
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-	char	*put_str;
+	t_str	str;
 	char	*head;
 	size_t	return_num;
 	size_t	i;
 
-	put_str = (char *)calloc(((size_t)INT_MAX + 1), sizeof(char));
-	if (put_str == NULL)
+	str.put_str = (char *)calloc(((size_t)INT_MAX + 1), sizeof(char));
+	if (str.put_str == NULL)
 		return (0);
-	head = put_str;
+	head = str.put_str;
 	va_start(ap, format);
 	i = 0;
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 			//csdixXpの対応をする
-			ap_add_str(&put_str, &format[++i], &ap);
+			ap_add_str(&str, &format[++i], &ap);
 		else
 			//文字をput_strに格納する
-			char_add_str(&put_str, format[i]);
+			char_add_str(&str, format[i]);
 		i++;
 	}
 	va_end(ap);
